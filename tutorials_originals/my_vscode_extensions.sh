e4dbb833aee91b9931e8c4aefa5c1d93ca3a033d
g1nj0L <178964499+g1nj0L@users.noreply.github.com>
2025-11-25 14:06:51 +0100

    Modificació my_vscode_extensions.sh

---
+++ tutorials_originals/my_vscode_extensions.sh
@@ -0,0 +1,30 @@
+# Export VS codium packages on PowerShell using codium --list-extensions | % { "codium --install-extension $_" }
+# Install all of the VS codium extensions below on bash or git bash with bash my_vscodium_extensions.sh
+
+# ADDING A COMMENT
+# made a change to test commits
+
+# Aquestes són les que recomana, jo sincerament passe d'instal·lar la majoria...
+
+codium --install-extension codiumzombiech.gitignore
+codium --install-extension ctctklfh.conda-env-activator
+codium --install-extension donjayamanne.git-extension-pack
+codium --install-extension donjayamanne.githistory
+codium --install-extension eamodio.gitlens
+codium --install-extension felipecaputo.git-project-manager
+codium --install-extension github.copilot
+codium --install-extension github.copilot-chat
+codium --install-extension jojoee.conda-cheatsheet
+codium --install-extension mccarter.start-git-bash
+codium --install-extension ms-ceintl.vscodium-language-pack-es
+codium --install-extension ms-python.debugpy
+codium --install-extension ms-python.python
+codium --install-extension ms-python.vscodium-pylance
+codium --install-extension ms-toolsai.jupyter
+codium --install-extension ms-toolsai.jupyter-keymap
+codium --install-extension ms-toolsai.jupyter-renderers
+codium --install-extension ms-toolsai.vscodium-jupyter-cell-tags
+codium --install-extension ms-toolsai.vscodium-jupyter-slideshow
+codium --install-extension phind.phind
+codium --install-extension slightc.pip-manager
+codium --install-extension ziyasal.vscodium-open-in-github
